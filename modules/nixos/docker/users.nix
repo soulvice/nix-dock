d18cf70717dc02ac837c904d1ead7dd9d7859b59
soulvice <admin@w0lf.io>
2025-10-14 23:37:59 +1000

    Secrets are fighting me

--- modules/nixos/docker/users.nix
+++ modules/nixos/docker/users.nix
@@ -6,9 +6,9 @@
     extraGroups = [ "wheel" "docker" "networkmanager" "systemd-journal" ];
     hashedPassword = "!";
     createHome = true;
-    openssh.authorizedKeys.keyFiles = [
-      "${config.age.secrets.ssh-key-docker.path}"
-    ];
+    #openssh.authorizedKeys.keyFiles = [
+    #  "${config.age.secrets.ssh-key-docker.path}"
+    #];
   };
 
   security.sudo.wheelNeedsPassword = false;
