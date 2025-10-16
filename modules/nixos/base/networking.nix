413d4c82e29396af38fdff9349cb1fa0afe5221e
soulvice <admin@w0lf.io>
2025-10-17 00:11:47 +1000

    Changes to networking

--- modules/nixos/base/networking.nix
+++ modules/nixos/base/networking.nix
@@ -25,9 +25,11 @@
 
   services.resolved = {
     enable = true;
+    dnssec = false;
     extraConfig = ''
       ${if hostname == "dock01" then "DNS=127.0.0.1" else "DNS=10.0.0.1"}
       FallbackDNS=100.100.100.100
+      DNSStubListener=yes
     '';
   };
 }
