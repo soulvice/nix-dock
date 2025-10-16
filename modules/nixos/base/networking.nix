ef1468f1c06c6fa22eeb08016091889ad2254987
soulvice <admin@w0lf.io>
2025-10-17 00:12:45 +1000

    Networking

--- modules/nixos/base/networking.nix
+++ modules/nixos/base/networking.nix
@@ -25,7 +25,7 @@
 
   services.resolved = {
     enable = true;
-    dnssec = false;
+    dnssec = "false";
     extraConfig = ''
       ${if hostname == "dock01" then "DNS=127.0.0.1" else "DNS=10.0.0.1"}
       FallbackDNS=100.100.100.100
