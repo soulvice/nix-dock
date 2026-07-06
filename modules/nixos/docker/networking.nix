a30ba34d8df42eb4d3636a2c0d79740aa1e992db
Soulvice <soulvice@users.noreply.github.com>
2026-07-07 08:34:21 +1000

    fix: docker network changes

--- modules/nixos/docker/networking.nix
+++ modules/nixos/docker/networking.nix
@@ -1,17 +1,39 @@
-{ ... }:
+{ config, lib, hostname, ... }:
 {
-
   # NETWORK INTERFACES
   # ens18 - Management NIC (tagged vlan10)
   # ens19 - Untagged NIC used for network communication for services.
   #         VLan interfaces are created with ens19 as the parent (ie: ens19.40 for tagged 40)
 
-  # Network Configuration
   networking.useDHCP = false;
+
   networking.interfaces.ens18 = {
     useDHCP = true;
   };
+
   networking.interfaces.ens19 = {
     useDHCP = false;
+  };
+
+  networking.macvlans = lib.mkIf (hostname == "dock01") {
+    macvlan-mgmt = {
+      interface = "ens18";
+      mode = "bridge";
+    };
+  };
+
+  networking.interfaces = lib.mkIf (hostname == "dock01") {
+    macvlan-mgmt = {
+      ipv4.addresses = [{ address = "10.0.0.254"; prefixLength = 32; }];
+      ipv6.addresses = [{ address = "fd0a:0:1::fffe"; prefixLength = 128; }];
+    };
+  };
+
+  systemd.network.networks."40-macvlan-mgmt" = lib.mkIf (hostname == "dock01") {
+    matchConfig.Name = "macvlan-mgmt";
+    routes = [
+      { routeConfig = { Destination = "10.0.0.0/23"; Scope = "link"; }; }
+      { routeConfig = { Destination = "fd0a:0:1::/64"; Scope = "link"; }; }
+    ];
   };
 }
